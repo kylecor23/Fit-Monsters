684a9649db16d7dad0e7836e4db60dddde57579b
KyleCorcoran <kylecorcoran9494@gmail.com>
2024-02-15 21:50:33 -0330

    worked on css styling

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import React, { useContext, useState } from "react";
-=======
-import { useContext, useState, useCallback } from "react";
-import Popup from "../components/popup-menu";
->>>>>>> 76313d77a3765d7ab52ee102ec7aa715dbea748c
 import { Link } from "react-router-dom";
 import MonstersInternalGoalTracker from "../components/MonsterInternalGoal";
 import StatsContext from "../components/StatsContex";
@@ -12,67 +7,33 @@ import JournalEntryInput from "../components/Journal.JSX";
 import MeditationTimer from "../components/MeditationTimer";
 
 export default function DashBoard() {
-  const { steps, calories, meditation } = useContext(StatsContext);
-  const [showJournalModal, setShowJournalModal] = useState(false);
-  const [showMeditationModal, setShowMeditationModal] = useState(false);
+	const { steps, calories, meditation, journal } = useContext(StatsContext);
+	const [showJournalModal, setShowJournalModal] = useState(false);
+	const [showMeditationModal, setShowMeditationModal] = useState(false);
 
-  const fitnessProgress = useCallback(
-    () => Math.min((steps / 8000) * 100, 100),
-    [steps]
-  );
-  const healthProgress = useCallback(
-    () => Math.min((calories / 2500) * 100, 100),
-    [calories]
-  );
-  const mindProgress = useCallback(
-    () => Math.min((meditation / 10) * 100, 100),
-    [meditation]
-  );
+	const toggleJournalModal = () => {
+		setShowJournalModal(!showJournalModal);
+	};
 
-  return (
-    <>
-      <header>
-        <h1>Fit Monsters</h1>
-        <nav className="nav">
-          <Popup>
-            <ul>
-              <li>
-                <Link to="/stats">Stats</Link>
-              </li>
-              <li>
-                <Link to="/challenges">Challenges</Link>
-              </li>
-            </ul>
-          </Popup>
-        </nav>
-      </header>
+	const toggleMeditationModal = () => {
+		setShowMeditationModal(!showMeditationModal);
+	};
 
-      <main>
-        <div className="dashboard">
-          <h1>dashboard</h1>
-          <div className="dasboard">
-            <img
-              src="https://media.istockphoto.com/id/1486708389/vector/cute-ghost-mascot-illustration-doing-weightlifting-illustration-of-a-ghost-doing-sports.jpg?s=612x612&w=0&k=20&c=e1aWV1pywLvXYqJcgiBz8zw1YciDGEvZ8cLHoE6uUR0="
-              alt="fit monster"
-            />
-          </div>
-          <div className="challenges">
-            <MonstersInternalGoalTracker
-              goalType="fitness"
-              progress={fitnessProgress}
-            >
-              <StepsInputField activity="steps" />
-            </MonstersInternalGoalTracker>
+	const fitnessProgress = Math.min((steps / 8000) * 100, 100);
+	const healthProgress = Math.min((calories / 2500) * 100, 100);
+	const mindProgress = Math.min((meditation / 10) * 100, 100);
 
-<<<<<<< HEAD
 	return (
 		<>
-			<header>
-				<h1>Fit Monsters</h1>
-			</header>
 			<div className="container">
 				<aside className="nav">
+					<header>
+						<h1>Fit Monsters</h1>
+					</header>
 					<ul>
+						<li>
+							<Link to="/dashboard">Home</Link>
+						</li>
 						<li>
 							<Link to="/stats">Stats</Link>
 						</li>
@@ -81,108 +42,58 @@ export default function DashBoard() {
 						</li>
 					</ul>
 				</aside>
-				<main>
-					<div className="dashboard">
-						<h1>dashboard</h1>
-						<div className="dasboard">
-							<img
-								src="https://media.istockphoto.com/id/1486708389/vector/cute-ghost-mascot-illustration-doing-weightlifting-illustration-of-a-ghost-doing-sports.jpg?s=612x612&w=0&k=20&c=e1aWV1pywLvXYqJcgiBz8zw1YciDGEvZ8cLHoE6uUR0="
-								alt="fit monster"
-							/>
-						</div>
-						<div className="challenges">
-							<MonstersInternalGoalTracker
-								goalType="fitness"
-								progress={fitnessProgress}
-							>
-								<StepsInputField activity="steps" />
-							</MonstersInternalGoalTracker>
+				<main className="dashboard">
+					<div className="dasboard">
+						<img
+							src="https://media.istockphoto.com/id/1486708389/vector/cute-ghost-mascot-illustration-doing-weightlifting-illustration-of-a-ghost-doing-sports.jpg?s=612x612&w=0&k=20&c=e1aWV1pywLvXYqJcgiBz8zw1YciDGEvZ8cLHoE6uUR0="
+							alt="fit monster"
+						/>
+					</div>
+					<div className="challenges">
+						<MonstersInternalGoalTracker
+							goalType="fitness"
+							progress={fitnessProgress}
+						>
+							<StepsInputField activity="steps" />
+						</MonstersInternalGoalTracker>
 
-							<MonstersInternalGoalTracker
-								goalType="health"
-								progress={healthProgress}
-							>
-								<StepsInputField activity="calories" />
-								<StepsInputField activity="weight" />
-							</MonstersInternalGoalTracker>
+						<MonstersInternalGoalTracker
+							goalType="health"
+							progress={healthProgress}
+						>
+							<StepsInputField activity="calories" />
+							<StepsInputField activity="weight" />
+						</MonstersInternalGoalTracker>
 
-							<MonstersInternalGoalTracker
-								goalType="mind"
-								progress={mindProgress}
-							>
-								<button className="modalButton" onClick={toggleJournalModal}>
-									Open Journal
-								</button>
+						<MonstersInternalGoalTracker
+							goalType="mind"
+							progress={mindProgress}
+						>
+							<button className="modalButton" onClick={toggleJournalModal}>
+								Open Journal
+							</button>
 
-								<button className="modalButton" onClick={toggleMeditationModal}>
-									Open Meditation
-								</button>
+							<button className="modalButton" onClick={toggleMeditationModal}>
+								Open Meditation
+							</button>
 
-								{showJournalModal && (
-									<JournalEntryInput
-										showModal={showJournalModal}
-										onClose={toggleJournalModal}
-									/>
-								)}
+							{showJournalModal && (
+								<JournalEntryInput
+									showModal={showJournalModal}
+									onClose={toggleJournalModal}
+								/>
+							)}
 
-								{showMeditationModal && (
-									<MeditationTimer
-										showModal={showMeditationModal}
-										onClose={toggleMeditationModal}
-									/>
-								)}
-							</MonstersInternalGoalTracker>
-						</div>
+							{showMeditationModal && (
+								<MeditationTimer
+									showModal={showMeditationModal}
+									onClose={toggleMeditationModal}
+								/>
+							)}
+						</MonstersInternalGoalTracker>
 					</div>
 				</main>
 			</div>
 		</>
 	);
-=======
-            <MonstersInternalGoalTracker
-              goalType="health"
-              progress={healthProgress}
-            >
-              <StepsInputField activity="calories" />
-              <StepsInputField activity="weight" />
-            </MonstersInternalGoalTracker>
-
-            <MonstersInternalGoalTracker
-              goalType="mind"
-              progress={mindProgress}
-            >
-              <button
-                className="modalButton"
-                onClick={() => setShowJournalModal(!showJournalModal)}
-              >
-                Open Journal
-              </button>
-
-              <button
-                className="modalButton"
-                onClick={() => setShowMeditationModal(!showMeditationModal)}
-              >
-                Open Meditation
-              </button>
-
-              {showJournalModal && (
-                <JournalEntryInput
-                  showModal={showJournalModal}
-                  onClose={() => setShowMeditationModal(!showMeditationModal)}
-                />
-              )}
-
-              {showMeditationModal && (
-                <MeditationTimer
-                  showModal={showMeditationModal}
-                  onClose={() => setShowMeditationModal(!showMeditationModal)}
-                />
-              )}
-            </MonstersInternalGoalTracker>
-          </div>
-        </div>
-      </main>
-    </>
-  );
->>>>>>> 76313d77a3765d7ab52ee102ec7aa715dbea748c
 }
